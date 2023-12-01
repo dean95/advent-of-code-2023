9620be18d68ea0a5ed56e7def3c083d759b60a97
Dean Djermanovic <dean.djermanovic@five.agency>
2023-12-01 09:48:06 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,52 @@
-fun main() {
+private fun main() {
+    val wordToDigitMap = mapOf(
+        "one" to "1",
+        "two" to "2",
+        "three" to "3",
+        "four" to "4",
+        "five" to "5",
+        "six" to "6",
+        "seven" to "7",
+        "eight" to "8",
+        "nine" to "9"
+    )
+
+    fun findDigit(input: String, includeLetters: Boolean, first: Boolean): String {
+        val range = if (first) input.indices else input.lastIndex downTo 0
+        for (i in range) {
+            val char = input[i]
+            if (char.isDigit()) return char.toString()
+
+            if (!includeLetters) continue
+            for ((digitName, digitValue) in wordToDigitMap) {
+                if (i - digitName.length + 1 >= 0 && input.substring(i - digitName.length + 1..i) == digitName)
+                    return digitValue
+            }
+        }
+
+        throw IllegalStateException()
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        var result = 0
+        input.forEach {
+            val firstDigit = findDigit(it, includeLetters = false, first = true)
+            val lastDigit = findDigit(it, includeLetters = false, first = false)
+            result += "$firstDigit$lastDigit".toInt()
+        }
+        return result
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        var result = 0
+        input.forEach {
+            val firstDigit = findDigit(it, includeLetters = true, first = true)
+            val lastDigit = findDigit(it, includeLetters = true, first = false)
+            result += "$firstDigit$lastDigit".toInt()
+        }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+        return result
+    }
 
     val input = readInput("Day01")
     part1(input).println()
